cc8f3423eed116d67abeb980942b3b24a1c3b418
mukesh22584 <mks22584@gmail.com>
2019-10-08 15:43:37 +0530

    msm8998-common: add GoogleCamera

--- common.mk
+++ common.mk
@@ -148,8 +148,11 @@ PRODUCT_PACKAGES += \
     android.hardware.camera.provider@2.4-service \
     camera.device@1.0-impl \
     camera.device@3.2-impl \
-    libshim_camera \
-    Snap
+    libshim_camera
+
+# Prebuilts
+PRODUCT_PACKAGES += \
+    GoogleCamera \
 
 PRODUCT_PACKAGES += \
     vendor.qti.hardware.camera.device@1.0 \
