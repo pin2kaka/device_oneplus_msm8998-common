f0d5ccc6a8842be1eaf69b24d2dc9b5ee0b89a3d
mukesh22584 <mks22584@gmail.com>
2019-10-08 11:31:39 +0530

    msm8998-common: add hardware secure element 1.0 for ril

--- common.mk
+++ common.mk
@@ -392,6 +392,7 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     android.hardware.radio@1.2 \
     android.hardware.radio.config@1.0 \
+    android.hardware.secure_element@1.0 \
     librmnetctl \
     libxml2 \
     libprotobuf-cpp-full
